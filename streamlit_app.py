1deebe93bc2cc4b833585765e2d7831008667917
Jay Dhamecha <118816923+JayDhamecha@users.noreply.github.com>
2022-11-23 11:55:52 +0530

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.title('This is Jay Dhamecha.')
 streamlit.title('I am here to help you in every manner which can i do!!')
 streamlit.title('Lets Resolve your error for better output')
 streamlit.title('Figurative Analytics Pvt Ltd an Ahmedabad Analytical company with global rating')
+
+streamlit.text('Hey!!')
+streamlit.text('How are you?')
+streamlit.text('We are welcoming you!!')
