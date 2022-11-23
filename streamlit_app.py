9406e328160ae5a8e6dc79daac33e4701b7b2651
Jay Dhamecha <118816923+JayDhamecha@users.noreply.github.com>
2022-11-23 12:11:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 
 streamlit.title('This is Jay Dhamecha.')
-streamlit.title('I am here to help you in every manner which can i do!!')
-streamlit.title('Lets Resolve your error for better output')
-streamlit.header('Figurative Analytics Pvt Ltd an Ahmedabad Analytical company with global rating')
+streamlit.header('I am here to help you in every manner which can i do!!')
+streamlit.text('Lets Resolve your error for better output')
+streamlit.title('Figurative Analytics Pvt Ltd an Ahmedabad Analytical company with global rating')
 
-streamlit.text('Hey!!')
+streamlit.header('Hey!!')
 streamlit.text('How are you?')
-streamlit.text('We are welcoming you!!')
+streamlit.title('We are welcoming you!!')
